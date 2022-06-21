8c6bd2156ecd323547b89bfab759fbec51b608ad
Jude <judetochyokoye@gmail.com>
2022-06-21 23:16:42 +0100

    refactored

---
+++ boring.js
@@ -0,0 +1,7 @@
+function boring(){
+    let name = window.prompt("Your name: ");
+    let height = window.prompt("Your height in fts: ");
+    let country = window.prompt("Your country: ");
+    alert(`Your name is ${name}, you are ${height}fts in height, and you are from ${country}`);
+}
+boring();
